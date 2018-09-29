3adc3c031489c4fccbb61d774e9c2833ab1adc1e
svyatlo <svyatlo@list.ru>
2018-09-29 22:20:12 +0300

    All zeros were found

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let powerOfPrimeFactor = 0;
+  const arr = [];
+
+  for (let i = 2; i <= base; i++) {
+    if (base % i != 0) continue;
+    while (base % i == 0) {
+      base /= i;
+      powerOfPrimeFactor += 1;
+    }
+    
+    let primeFactor = i,
+        num = number,
+        numberOfZeros = 0;
+
+    while(num > 1) {
+      num = Math.floor(num / primeFactor);
+      numberOfZeros += num;
+    }
+
+    arr.push(Math.floor(numberOfZeros / powerOfPrimeFactor));
+    if (base == 1) break;
+    powerOfPrimeFactor = 0;
+  }
+  
+  arr.sort(function(a, b) { return a - b;});
+  return arr[0];
 }
